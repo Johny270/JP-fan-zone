6a300c656e66aa9ec8bcebf4efd2a9ccb8b8e08c
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-09-05 10:28:45 -0300

    Creates displayRatio and displayTimer functions

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 import { getRandomQuote } from "./quotes.js";
 
 /*------------ Constants ------------*/
-const roundQuoteArr = getRandomQuote();
+// const roundQuoteArr = getRandomQuote();
 const totalQuestions = 10;
+const minutes = "00";
 
 // console.log(roundQuoteArr); -- works
 
@@ -11,7 +12,10 @@ const totalQuestions = 10;
 /*------------ Variables ------------*/
 let ratioText, timerText;
 let ratio;
-let timeLeft = 20;
+let timeLeft;
+let roundQuoteArr = [];
+const arr = getRandomQuote();
+// console.log(arr)
 
 
 
@@ -31,7 +35,7 @@ const submitBtn = document.querySelector("#submit-btn");
 
 
 /*------------ Event listeners ------------*/
-playBtn.addEventListener("click", start);
+playBtn.addEventListener("click", displaytimer);
 submitBtn.addEventListener("click", updateRatio)
 
 
@@ -42,13 +46,15 @@ function start() {
 
   // initialize ratio to 1
   ratio = 1;
-  // reset timer to 0
-
+  // reset timer to 20 seconds
+  timeLeft = 20;
   // reset right/wrong ratio
 
   // display random quote in quotes placeholder
 
   // disable the submit button
+
+  // Clear input field and put focus in it
 
   // render game state to the player
 }
@@ -58,10 +64,23 @@ function render() {
 }
 
 function displaytimer() {
+  timeLeft = 20;
+  let timer = setInterval(function() {
+    // Display timer in the format 'min : sec' in the timer container
+    timerContainer.textContent = minutes + " : " + `${timeLeft}`;
+    timeLeft--;
+    if(timeLeft < 0) {
+      timerContainer.textContent = minutes + " : " + minutes;
+      clearInterval(timer);
+      // submit what's in the input field when timer hits zero
+      submitBtn.click();
+    }
+  }, 1000);
 
 }
 
 function updateRatio() {
+  ratio = 1;
   // Increment ratio by one each time the submit button gets clicked
   ratio++;
 
@@ -72,10 +91,36 @@ function updateRatio() {
 
   // display updated ratio to the screen
   ratioContainer.innerHTML = `${ratio} / ${totalQuestions}`;
+
+  // Store the current value of ratio for next round
+
+
   // console.log("submit was clicked!")
   // console.log(ratioContainer.textContent)
 }
 
+function renderQuote() {
+  // Implementing a Fisher-Yates Sorting Algorithm
+  const shuffle = (arr) => {
+    for(let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  };
+  let shuffledArray = [];
+  shuffledArray = shuffle(arr);
+  return  console.log(shuffledArray);
+}
+renderQuote();
+
 function updatePlayBtn() {
 
 }
+
+function submit() {
+
+}
+
+// function reset() {
+//   start();
+// }
