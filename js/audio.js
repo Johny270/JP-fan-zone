a7ad070ef5513f153614eee39ab7146c27afbe39
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-09-08 11:26:56 -0300

    add a new song and work css

--- js/audio.js
+++ js/audio.js
@@ -3,7 +3,7 @@ let narutoKakuzu = new Audio("../assets/naruto-sountrack_kakuzu-fight.mp3");
 let narutoPein = new Audio("../assets/naruto-sountrack_pein-tendok.mp3");
 let snk2 = new Audio("../assets/attack-on-titan.mp3");
 let codeGeass = new Audio("../assets/stories.mp3");
-// let mha = new Audio("../assets")
+let mha = new Audio("../assets/Deku and Katsuma.mp3")
 
 const jpSoundtrack = [];
 
@@ -12,6 +12,7 @@ jpSoundtrack.push(narutoKakuzu);
 jpSoundtrack.push(narutoPein);
 jpSoundtrack.push(snk2);
 jpSoundtrack.push(codeGeass);
+jpSoundtrack.push(mha);
 
 function getSongs() {
   return jpSoundtrack;
