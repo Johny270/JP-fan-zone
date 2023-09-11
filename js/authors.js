417d9f2e4c40b1b5992b378d73c5cee66d4b9ab6
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-09-11 02:52:40 -0300

    add guessTitle.html, guessTitle.css, authors.js, guessTitle.js to handle the second game category

---
+++ js/authors.js
@@ -0,0 +1,12 @@
+const animeInfo = [
+
+  {
+    authorName: "Hajime Issayama",
+    animeTitle: "Attack on Titans",
+    date: 2011, 
+  }
+
+]
+
+animeInfo[0].image = "../assets/anime-cover (3).jpg";
+console.log(animeInfo[0]);
