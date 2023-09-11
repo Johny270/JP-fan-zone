4f6d4b3a19db69a1335fd2b1bc11fcb4ac547c6f
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-09-11 03:09:07 -0300

    Add assets folder in the assets folder for guessTitle images

--- js/authors.js
+++ js/authors.js
@@ -3,9 +3,93 @@ const animeInfo = [
   {
     authorName: "Hajime Issayama",
     animeTitle: "Attack on Titans",
+    date: 2009, 
+  },
+  {
+    authorName: "Masashi Kishimoto",
+    animeTitle: "Naruto",
+    date: 1999, 
+  },
+  {
+    authorName: "Akira Toriyama",
+    animeTitle: "Dragon Ball",
+    date: 1984, 
+  },
+  {
+    authorName: "Eichiro Oda",
+    animeTitle: "One Piece",
+    date: 1997, 
+  },
+  {
+    authorName: "Tite Kubo",
+    animeTitle: "Bleach",
+    date: 2001, 
+  },
+  {
+    authorName: "Sui Ishida",
+    animeTitle: "Tokyo Ghoul",
     date: 2011, 
-  }
-
+  },
+  {
+    authorName: "Takeshi Obata & Tsugumi Obha",
+    animeTitle: "Bakuman",
+    date: 2008, 
+  },
+  {
+    authorName: "Takeshi Obata & Tsugumi Obha",
+    animeTitle: "Death Note",
+    date: 2003, 
+  },
+  {
+    authorName: "Takehiko Inoue",
+    animeTitle: "Vagabond",
+    date: 19998, 
+  },
+  {
+    authorName: "Takehiko Inoue",
+    animeTitle: "Slam Dunk",
+    date: 1991, 
+  },
+  {
+    authorName: "Gege Akutami",
+    animeTitle: "Jujutsu Kaisen",
+    date: 2018, 
+  },
+  {
+    authorName: "Hirohiko Araki",
+    animeTitle: "Jojo Bizarre Adventures",
+    date: 1987, 
+  },
+  {
+    authorName: "Yoshitoki Oima",
+    animeTitle: "A silent voice",
+    date: 2011, 
+  },
+  {
+    authorName: "Junji Ito",
+    animeTitle: "Uzumaki",
+    date: 2010, 
+  },
+  {
+    authorName: "Junji Ito",
+    animeTitle: "No longer Human",
+    date: 2019, 
+  },
+  {
+    authorName: "Yoichi Takahashi",
+    animeTitle: "Captain Tsubasa",
+    date: 1981, 
+  },
+  {
+    authorName: "Kazuki Takahashi",
+    animeTitle: "Yugi-OH",
+    date: 1996, 
+  },
+  {
+    authorName: "Kafka Asagiri",
+    animeTitle: "Bungou Stray Dogs",
+    date: 2012, 
+  },
 ]
 
 animeInfo[0].image = "../assets/anime-cover (3).jpg";
