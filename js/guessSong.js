c2bad82add66725f5a7538cbef7d55867bcc149f
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-09-12 22:53:45 -0300

    build guessSong game by defining all its functions

--- js/guessSong.js
+++ js/guessSong.js
@@ -5,23 +5,100 @@ const songs = getSongs2();
 console.log(songs);
 
 /*------------ variables ------------*/
-let resultMes;
+let song;
+let options = [];
+let shuffledOptions = [];
+let timeLeft;
+let timer;
 
 /*------------ Cached Elements Reference ------------*/
-const playBtn = document.querySelector("play-btn");
-const resultContainer = document.querySelector("instructions");
-const option1 = document.querySelector("option1");
-const option2 = document.querySelector("option2");
-const option3 = document.querySelector("option3");
-const resetBtn = document.querySelector("reset");
-const exitBtn = document.querySelector("exit");
+const playBtn = document.querySelector("#play-btn");
+const resultContainer = document.querySelector("#instructions");
+const option1 = document.querySelector("#option1");
+const option2 = document.querySelector("#option2");
+const option3 = document.querySelector("#option3");
+const resetBtn = document.querySelector("#reset");
+const exitBtn = document.querySelector("#exit");
+const optionsContainer = document.querySelector("#options");
 
 
 /*------------ Event Listeners ------------*/
-
+playBtn.addEventListener("click", play);
+resetBtn.addEventListener("click", reset);
+optionsContainer.addEventListener("click", printResults);
 
 /*------------ Functions ------------*/
 
-function playSong() {
+options.push(option1);
+options.push(option2);
+options.push(option3);
 
+function play() {
+  render();
 }
+
+function render() {
+  playSong();
+  shuffleOptions();
+  displayOptions()
+}
+
+function playSong() {
+  song = songs[Math.floor(Math.random() * songs.length)];
+  song.song.play();
+}
+
+function shuffleOptions() {
+  const shuffle = (array) => {
+    for(let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+  shuffledOptions = shuffle(songs);
+  return shuffledOptions;
+}
+
+function displayOptions() {
+  options[Math.floor(Math.random() * options.length)].textContent = song.songTitle;
+  for(let i = 0; i < options.length; i++) {
+    if (options[i].textContent === "") {
+      options[i].textContent = shuffledOptions[Math.floor(Math.random() * shuffledOptions.length)].songTitle;
+    }
+  }
+}
+
+function printResults(evt) {
+  timeLeft = 2;
+  timer = setInterval(function() {  
+    if(evt.target.textContent === song.songTitle){
+      resultContainer.textContent = "CORRECT !";
+    } else {
+      resultContainer.textContent = "WRONG !";
+    }
+    timeLeft--;
+    if(timeLeft < 0) {
+      clearInterval(timer);
+      song.song.pause();
+      resultContainer.textContent = "";
+      render();
+    }
+  }, 1000);
+}
+
+function reset() {
+  clearInterval(timer);
+  song.song.pause();
+  resultContainer.textContent = "";
+  option1.textContent = "";
+  option2.textContent = "";
+  option3.textContent = "";
+  playBtn.disabled = false;
+}
+
+
+
+
+
+
